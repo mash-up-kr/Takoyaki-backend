3ae3322af2d8dcc70dc8d05b230764156463852f
codingsquid <gksxodnd007@gmail.com>
2018-06-28 16:25:24 +0900

    feat: Web이니셜라이저 등록

---
+++ src/main/java/org/mashup/takoyaki/configs/WebInitializer.java
@@ -0,0 +1,25 @@
+package org.mashup.takoyaki.configs;
+
+import org.springframework.web.WebApplicationInitializer;
+import org.springframework.web.context.ContextLoaderListener;
+import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
+import org.springframework.web.servlet.DispatcherServlet;
+
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRegistration;
+
+public class WebInitializer implements WebApplicationInitializer {
+
+    @Override
+    public void onStartup(ServletContext servletContext) throws ServletException {
+        AnnotationConfigWebApplicationContext rootContext = new AnnotationConfigWebApplicationContext();
+        rootContext.register(RootContextConfig.class);
+
+        servletContext.addListener(new ContextLoaderListener(rootContext));
+        ServletRegistration.Dynamic dispatcher = servletContext.addServlet("dispatcherServlet", new DispatcherServlet(rootContext));
+
+        dispatcher.setLoadOnStartup(1);
+        dispatcher.addMapping("/takoyaki");
+    }
+}
