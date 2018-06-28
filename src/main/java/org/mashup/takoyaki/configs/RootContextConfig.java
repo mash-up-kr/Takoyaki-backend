c531302d7cce9fc6cdaa2931c528b9f4e9c1a73a
codingsquid <gksxodnd007@gmail.com>
2018-06-28 15:33:56 +0900

    feat: 초기 커밋
    spring web mvc 프로젝트 설정

---
+++ src/main/java/org/mashup/takoyaki/configs/RootContextConfig.java
@@ -0,0 +1,17 @@
+package org.mashup.takoyaki.configs;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.FilterType;
+import org.springframework.context.annotation.Import;
+
+@Configuration
+@ComponentScan(basePackages = "org.mashup.takoyaki",
+        excludeFilters =  {
+        @ComponentScan.Filter(type = FilterType.ASSIGNABLE_TYPE,
+            value = { DataSourceConfig.class, WebMvcConfig.class })
+        })
+@Import(value = { DataSourceConfig.class, WebMvcConfig.class })
+public class RootContextConfig {
+
+}
---
+++ src/main/java/org/mashup/takoyaki/configs/WebMvcConfig.java
@@ -0,0 +1,11 @@
+package org.mashup.takoyaki.configs;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+@Configuration
+@EnableWebMvc
+public class WebMvcConfig extends WebMvcConfigurerAdapter {
+
+}
