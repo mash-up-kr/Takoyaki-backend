63351c0e85f5982a8e5a73b67f34128b8c681003
hubert.squid <hubert.squid@kakaopaycorp.com>
2018-07-28 19:47:38 +0900

    style: 사용되지않는 클래스가 import되어있어 이를 삭제

--- src/main/java/org/mashup/takoyaki/configs/WebMvcConfig.java
+++ src/main/java/org/mashup/takoyaki/configs/WebMvcConfig.java
@@ -3,12 +3,11 @@ package org.mashup.takoyaki.configs;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @Configuration
 @EnableWebMvc
 public class WebMvcConfig implements WebMvcConfigurer {
 
-    
+
 
 }
