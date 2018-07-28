8ec326ca726eed4f976a40c4ad75150f9ab751ba
hubert.squid <hubert.squid@kakaopaycorp.com>
2018-07-28 19:45:56 +0900

    WebMvcConfig 클래스 수정
    Spring 5.xx WebMvcConfigurerAdapter 추상클래스가 deprecate됨. java8의 문법을 적극 활용하기위해 WebMvcConfigurer 인터페이스에 default method로 정의 해놨기 때문임. 이에따라 warning을 없애기 위해 수정함.

--- src/main/java/org/mashup/takoyaki/configs/WebMvcConfig.java
+++ src/main/java/org/mashup/takoyaki/configs/WebMvcConfig.java
@@ -2,10 +2,13 @@ package org.mashup.takoyaki.configs;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @Configuration
 @EnableWebMvc
-public class WebMvcConfig extends WebMvcConfigurerAdapter {
+public class WebMvcConfig implements WebMvcConfigurer {
+
+    
 
 }
