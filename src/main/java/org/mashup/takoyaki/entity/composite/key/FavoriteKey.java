5c505323b22157a0c8ef29de9e484ca8a40e1637
hubert.squid <hubert.squid@kakaopaycorp.com>
2018-07-29 15:33:25 +0900

    feat: 복합키 등록

--- src/main/java/org/mashup/takoyaki/entity/composite/key/FavoriteKey.java
+++ src/main/java/org/mashup/takoyaki/entity/composite/key/FavoriteKey.java
@@ -1,4 +1,27 @@
 package org.mashup.takoyaki.entity.composite.key;
 
-public class FavoriteKey {
+import lombok.*;
+import org.mashup.takoyaki.entity.Report;
+import org.mashup.takoyaki.entity.User;
+
+import javax.persistence.Embeddable;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import java.io.Serializable;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@EqualsAndHashCode
+@Getter @Setter
+@Embeddable
+public class FavoriteKey implements Serializable {
+
+    @ManyToOne
+    @JoinColumn(name = "user_id", nullable = false)
+    private User userId;
+
+    @ManyToOne
+    @JoinColumn(name = "report_id", nullable = false)
+    private Report reportId;
+
 }
