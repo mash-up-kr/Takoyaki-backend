214f391c473b493f62d89bd5415ce0947028ff73
hubert.squid <hubert.squid@kakaopaycorp.com>
2018-08-11 13:07:51 +0900

    feat: user Entity 수정
    1. 토큰 발급을 위함
    - 토큰 발급시 토큰의 생성 시간 데이터도 필요하다고 판단
    2. user가 자신의 프로필을 수정하는 상황 고려
    - 프로필 업데이트시 업데이트 시간 데이터도 필요하다고 판단
    - 처음 부여 nickname은 동일하게 가는 것이 나아보인다고 판단. 유니크한 nickname을 원하시 유저가 변경 해야함.

--- src/main/java/org/mashup/takoyaki/entity/User.java
+++ src/main/java/org/mashup/takoyaki/entity/User.java
@@ -1,29 +1,38 @@
 package org.mashup.takoyaki.entity;
 
-import lombok.Data;
+import lombok.*;
+import org.mashup.takoyaki.entity.value.AccessToken;
 
 import javax.persistence.*;
 import java.time.LocalDateTime;
 import java.util.List;
 
+@Getter @Setter
+@ToString(exclude = "reportList")
+@EqualsAndHashCode
 @Entity
 @Table(name = "TB_USERS",
         uniqueConstraints = @UniqueConstraint(columnNames = "access_token"))
-@Data
 public class User {
 
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
 
-    @Column(name = "access_token", nullable = false)
-    private String accessToken;
+    @Embedded
+    private AccessToken accessToken;
 
     @Column(name = "email")
     private String eamil;
 
+    @Column(name = "nickname", nullable = false)
+    private String nickname;
+
     @Column(name = "created_at", nullable = false)
     private LocalDateTime createdAt;
+
+    @Column(name = "updated_at", nullable = false)
+    private LocalDateTime updatedAt;
 
     @Column(name = "profile_image_url")
     private String profileImageUrl;
@@ -33,7 +42,16 @@ public class User {
 
     @PrePersist
     public void initData() {
-        this.createdAt = LocalDateTime.now();
+        //TODO nickname 발급 로직이 필요합니다.
+        this.nickname = "TAKOYAKI";
+        LocalDateTime now = LocalDateTime.now();
+        this.createdAt = now;
+        this.updatedAt = now;
+    }
+
+    @PreUpdate
+    public void initUpdateDate() {
+        this.updatedAt = LocalDateTime.now();
     }
 
 }
