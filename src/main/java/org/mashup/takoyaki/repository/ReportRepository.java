08b6564d2e99f32ec03a1b5347ff3dada46cfb89
chaeeun <chaeeun218@gmail.com>
2018-08-11 13:03:12 +0900

    feat: ReportRepository 추가

---
+++ src/main/java/org/mashup/takoyaki/repository/ReportRepository.java
@@ -0,0 +1,8 @@
+package org.mashup.takoyaki.repository;
+
+import org.mashup.takoyaki.entity.Report;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ReportRepository extends JpaRepository<Report, Long> {
+
+}
