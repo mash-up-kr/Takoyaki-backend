00254c6ccadb0152fc8d15b2d605f7419604edc2
hubert.squid <hubert.squid@kakaopaycorp.com>
2018-08-12 02:02:43 +0900

    feat: java LocalDateTime 메시지 컨버터 등록
    모든 클래스의 LocalDateTime 타입의 필드를 ISO-8601 format으로 출력하기 위한 설정

--- src/main/java/org/mashup/takoyaki/entity/value/AccessToken.java
+++ src/main/java/org/mashup/takoyaki/entity/value/AccessToken.java
@@ -14,7 +14,6 @@ import java.time.LocalDateTime;
  * @author 한태웅
  * @since 2018-08-11
  */
-//TODO createdAt LocalDateTime으로 json serialize가 필요합니다.
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
@@ -25,7 +24,6 @@ public class AccessToken {
     private String token;
 
     @JsonProperty(value = "created_at")
-    @JsonFormat(pattern = "yyyy-MM-dd'T'hh:mm:ss")
     @Column(name = "token_created_at")
     private LocalDateTime createdAt;
 
