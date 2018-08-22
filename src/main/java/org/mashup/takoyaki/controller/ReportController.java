30cf4954ea95e205d92c686cefc13a83afdbfacb
chaeeun <chaeeun218@gmail.com>
2018-08-23 01:52:37 +0900

    feat: 제보하기 API 성공 응답 메시지 수정

--- src/main/java/org/mashup/takoyaki/controller/ReportController.java
+++ src/main/java/org/mashup/takoyaki/controller/ReportController.java
@@ -2,16 +2,14 @@ package org.mashup.takoyaki.controller;
 
 import lombok.extern.slf4j.Slf4j;
 import org.mashup.takoyaki.common.exception.BadRequestException;
-import org.mashup.takoyaki.common.exception.UnAuthorizedException;
-import org.mashup.takoyaki.common.model.ApiResponseModel;
 import org.mashup.takoyaki.dto.ReportDto;
-import org.mashup.takoyaki.entity.Report;
 import org.mashup.takoyaki.service.ReportService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
+import javax.servlet.http.HttpServletResponse;
 import java.time.LocalDate;
 
 @Slf4j
@@ -27,25 +25,21 @@ public class ReportController {
     }
 
     @PostMapping
-    public ApiResponseModel<Report> reportTruck(
-            @RequestHeader("Access-Token") String accessToken,
-            @RequestPart("photo_files")MultipartFile[] photoFiles, @RequestPart ReportDto report) {
+    public void reportTruck(@RequestHeader("Access-Token") String accessToken,
+                                                @RequestPart("photo_files")MultipartFile[] photoFiles,
+                                                @RequestPart ReportDto report,
+                                                HttpServletResponse response) {
 
         log.info("요청 받은 트럭 정보 : {}", report.toString());
 
-        if(photoFiles.length > 3) {
+        if (photoFiles.length > 3) {
             throw new BadRequestException();
         }
 
         if (LocalDate.now().isAfter(report.getExpirationDate())) {
             throw new BadRequestException();
         }
-
-        ApiResponseModel<Report> response = new ApiResponseModel<>();
-        response.setCode(HttpStatus.OK.value());
-        response.setMsg(HttpStatus.OK.toString());
         reportService.report(accessToken, photoFiles, report);
-        return response;
-
+        response.setStatus(HttpStatus.OK.value());
     }
 }
